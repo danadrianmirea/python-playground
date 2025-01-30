f6750192befbe6ccf118a597105841b6a2cfa582
Dan Adrian Mirea <damirea84@gmail.com>
2025-01-31 00:44:17 +0200

    add arhithmetic

---
+++ arithmetic.py
@@ -0,0 +1,17 @@
+count = 0
+count = count + 1
+count += 1
+count -= 2
+
+mul = 1
+mul = mul*3
+mul = mul/3
+mul *= 5
+#mul /= 2
+#mul = int(mul)
+mul = (int)(mul/2)
+mul = 1
+mul = 2
+mul **= 4
+
+print(f"count: {count}, mul: {mul}")
