294b714d82f7194ffd8c51bc25130ebda8e3f4e3
Dan Adrian Mirea <damirea84@gmail.com>
2025-01-31 01:55:14 +0200

    update with area of deez nuts

--- arithmetic.py
+++ arithmetic.py
@@ -28,3 +28,8 @@ print(f"res: {res}, resceil: {resceil}, resfloor: {resfloor}")
 x = 16
 ressqrt = math.sqrt(x)
 print(f"ressqrt: {ressqrt}")
+
+
+nutsRadius = float(input("Enter the radius of deez nuts: "))
+areaNuts = math.pi * pow(nutsRadius, 2)
+print(f"The area of deez nuts is: {areaNuts}")5
